bfd6e20c54a530c6a84e2b3c8e8ae8cfcc265649
Aldana GC <aldana.gusmeroli@hotmail.com>
2022-10-04 10:59:28 -0300

    carga nuevos archivos

---
+++ bucleDoWhile.java
@@ -0,0 +1,6 @@
+public class bucleDoWhile {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ bucleWhile.java
@@ -0,0 +1,20 @@
+
+public class bucleWhile {
+
+    public static void main(String[] args) {
+        
+        //Inicializo y declaro variable
+        int c = 0;
+        
+        //Bucle While
+
+        while(c <= 10){
+            //Bloque de sentencias
+            System.out.println(c);
+
+            //Actualizacion de la variable
+            c++;
+        }
+
+    }
+}
