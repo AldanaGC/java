106f8b1072c9aa63c79b74b3d07a3780dd547d29
Aldana GC <aldana.gusmeroli@hotmail.com>
2022-10-18 18:09:06 -0300

    arreglos cajero funcionalidades

---
+++ MiArrayList.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+/**
+ * MiArrayList
+ */
+import java.util.ArrayList;
+
+public class MiArrayList {
+
+    public static void main(String[] args) {
+        
+        //declaracion de array list
+        ArrayList<String> autos;
+
+        //instanciacion del AL
+        autos = new ArrayList<String>();
+
+        //metodo del AL
+        //1-Insertar con el metodo .add()
+        autos.add("Volvo");
+        autos.add("BMW");
+        autos.add("Fitito");
+        autos.add("Escarabajo");
+
+
+        //salida
+        System.out.println("El AL autos es " + autos);
+
+        //Obtener longitud del arraylist
+        //autos.size();
+        System.out.println("La longitud de autos es: " + autos.size());
+
+        //4- Añadimos elemento en cualquier momento
+        autos.add("Torino");
+
+        System.out.println("El AL con el nuevo elemento es " + autos);
+
+        //5- getters y setters get trae set modifica
+        //autos.get(indice);
+        System.out.println("El valor del elemento del indice 3 es: " +autos.get(3));
+
+        //autos.set(indice que quiero sobreescribir,"lo que quiero cambair/valor")
+        autos.set(1, "Taunus");
+        System.out.println("El nuevo AL con el index 1 cambiado es: " + autos);
+
+        //5- Obtener ultimo elemento de AL
+        //autos.size-1
+
+    }
+}
--- cajeroMejorado.java
+++ cajeroMejorado.java
@@ -16,7 +16,7 @@ import java.util.*;
  * @version 3.0
  */
 
-public class homeBankingMejorado{
+public class cajeroMejorado{
     public static void main(String[] args) {
         //declaro objeto
         Scanner miTeclado;
@@ -28,19 +28,27 @@ public class homeBankingMejorado{
         String usuario, usuarioGuardado, claveGuardada, clave, opcion, sesionContinuar, sesion;
         //modificar saldo para probar todas las funciones
         int saldo = 50000;
-        int monto, totalPagos, intentos, maximo, cbu;
-        char menu;
-        cbu = 0;
-        maximo = 3;
-        intentos = 1;
-        monto = 0;
-        menu = 'A';
+        int monto, totalPagos, intentos, maximo, cbu, menu, respuesta, rtaSaved;
+        //variables ingresos por teclado opciones
+        menu = 'A'; //para ir al menu de opciones
         opcion = "";
-        sesion = "";
+        sesion = "";//para continuar al menu principal
+        sesionContinuar = "S";
+        respuesta=0;
+        rtaSaved=1;
+        
+        //variables registro
         usuario="";
         clave = "";
         usuarioGuardado = "aldana";
         claveGuardada = "1234";
+
+
+        cbu = 0;
+        maximo = 3;
+        intentos = 1;
+        monto = 0;
+
         sesionContinuar = "S";
         int cablevision = 10000;
         int inmbobiliario = 4998;
@@ -69,15 +77,86 @@ public class homeBankingMejorado{
                 System.out.println("¿Desea continuar operando? Marque S para continuar al Menu de opciones / N para cerrar sesion)");
                 sesion = miTeclado.nextLine();
             }if(sesion .equals(sesionContinuar)){
-                System.out.println("Menu de opciones");
-                System.out.println("A: Realizar transferencia. B: Realizar un pago. C: Cerrar sesión");
-                menu = miTeclado.next().charAt(0);
-                System.out.println("Ha seleccionado la opcion: " + menu);
+                System.out.println("Cargando menú de opciones");
+                do{
+                do{
+                    System.out.println("Menú de opciones");
+                    System.out.println("1: Realizar transferencia.");
+                    System.out.println("2: Realizar un pago.");
+                    System.out.println("3: Cerrar sesión.");
+                    menu = miTeclado.nextInt();
+                    System.out.println("Ha seleccionado la opcion: " + menu);
+                    }while(menu<1 || menu>4);
+
+            //estructura switch
+                    switch (menu) {
+
+                        case 1:
+                            System.out.println("Seccion TRANSFERENCIAS");
+                            System.out.println("Ingrese monto a transferir");
+                            monto = miTeclado.nextInt();
+                            if(monto > saldo){
+                                System.out.println("No posee saldo suficiente para realizar esta operación");
+
+                            }else{
+                            System.out.println("Usted va a transferir: $" + monto + ". ¿Desea continuar? 1 (Continuar) / 2 (cancelar y regresar al menú anterior)");
+                            respuesta = miTeclado.nextInt();
+                                if(respuesta == rtaSaved){
+                                    System.out.println("Se debitarán de su cuenta: $" + monto);
+                                    System.out.println("Ingrese CBU DESTINATARIO");
+                                    cbu = miTeclado.nextInt();
+                                    System.out.println("*****A continuación verifique datos ingresados y confirme operación.*****");
+                                    System.out.println("Importe a tranferir $" +monto);
+                                    System.out.println("CBU DESTINTARIO:" +cbu);
+                                    System.out.println("**************************************");
+                                    System.out.println("1-Tranferir.");
+                                    System.out.println("2-Cancelar");
+                                    System.out.println("**************************************");
+                                    respuesta=miTeclado.nextInt();
+                                }if((respuesta == rtaSaved)){
+                                    System.out.println("Operación exitosa! Se han trasnferido $" + monto + " de su cuenta al CBU N° " + cbu +". Su saldo actual es de: $" + (saldo-monto));
+                                    System.out.println("**************************************");
+                                }else{
+                                    System.out.println("Operación cancelada");
+                                    System.out.println("**************************************");
+
+                                }
+                            }
+                            System.out.println("*****Aguarde y será redirigido al menu de opciones*****");
+                            System.out.println("**********************************************************");
+                            break;
+                        case 2://PAGO DE SERVICIOS
+                            System.out.println("Seccion PAGOS");
+                            System.out.println("Pendiente de pago - CABLEVISION FIBERTEL. Monto a abonar: $" + cablevision);
+                            System.out.println("Pendiente de pago - IMPUESTO INMOBILIARIO. Monto a abonar: $"+ inmbobiliario);
+                            System.out.println("Pendiente de pago - IMPUESTO AUTOMOTOR. Monto a abonar: $" + automotor);
+                            System.out.println("El monto total a pagar es de: $" + totalPagos);
+                            System.out.println("¿Desea proceder al pago de lo mismos? 1- para Continuar / 2- para terminar");
+                            respuesta = miTeclado.nextInt();
+                            if(respuesta == rtaSaved){
+                                System.out.println("Se debitarán de su cuenta: $" + totalPagos);
+                                System.out.println("1- Confirmar y pagar.");
+                                System.out.println("1- Cancelar y regresar el menú anterior.");
+
+                                respuesta=miTeclado.nextInt();
+                            }if((totalPagos <= saldo ) && (respuesta == rtaSaved)){
+                                System.out.println("Pagos realizados con éxito, se debitó de su cuenta la suma de $" + totalPagos + ". Su saldo actual es: $" +  (saldo - totalPagos));
+                            }else if (saldo<totalPagos){
+                                System.out.println("Su saldo es insuficiente para realizar la transaccion.");
+                            }
+                                break;
+                        case 3:
+                            System.out.println("Gracias por utilizar nuestro sistema de HB. Hasta pronto.");
+                            break;
+                       }
+                    }while((menu != 3) && (sesion != sesionContinuar));
+            }else{
+                System.out.println("Cerrando sesion");
             }
 
 
         //ESTRUCTURA SWITCH para menu de opciones trasnferencia/pagos/salir
-       switch (menu) {
+       /*switch (menu) {
 
         case 'A':
             System.out.println("Seccion TRANSFERENCIAS");
@@ -123,7 +202,7 @@ public class homeBankingMejorado{
         default:
             System.out.println("Su sesión ha expirado. Ingrese nuevamente sus datos para entrar al sistema");
             break;
-       }
+       }/* */
        System.out.println("Gracias por utilizar nuestro sistema de HB. Hasta pronto.");
 
 
