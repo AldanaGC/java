8a8642c3722d7e2c6afd5c0c77f59ba452fb4f1c
Aldana GC <aldana.gusmeroli@hotmail.com>
2022-11-29 16:18:44 -0300

    nueva carpeta clase 30, metodo random

---
+++ Activ clases/clase30/MetodoRandom.java
@@ -0,0 +1,11 @@
+package clase30;
+
+public class MetodoRandom {
+    public static void main(String[] args) {
+        
+        //Metodo Random
+        System.out.println(Math.random());
+        
+    }
+    
+}
